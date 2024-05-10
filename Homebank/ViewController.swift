f5de767e95e52fdd80044881beb6d92e8d643d51
Nurgazy Shyrynkhan <nurgazy240598@gmail.com>
2024-05-10 09:30:43 +0500

    Initial Commit

---
+++ Homebank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homebank
+//
+//  Created by Gazinho Dos Santos on 10.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
