632c0850fdc9b41d4445a1985c254427d203268c
Nurgazy Shyrynkhan <nurgazy240598@gmail.com>
2024-06-14 13:44:26 +0500

    add transfersVC

---
+++ Homebank/Controllers/Extra/TransfersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TransfersViewController.swift
+//  Homebank
+//
+//  Created by Gazinho Dos Santos on 14.06.2024.
+//
+
+import UIKit
+
+final class TransfersViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AppColor.grey01.uiColor
+    }
+}
